dfe0f25459d091bc06f772e793dc3be1041a6c35
Doug Bakewell <dbakewel@gmail.com>
2019-04-11 16:51:42 -0700

    Switch using python launcher in rundemo.bat file: "py -3" rather than "python"

--- rundemo.bat
+++ rundemo.bat
@@ -1,8 +1,8 @@
-start "NetBot-Server" cmd /K python src/netbots_server.py -p 20000 -stepsec 0.05 -games 10
+start "NetBot-Server" cmd /K py -3 src/netbots_server.py -p 20000 -stepsec 0.05 -games 10
 
-start "hideincorner" cmd /K python robots/hideincorner.py -p 20002 -sp 20000
-start "lighthouse" cmd /K python robots/lighthouse.py -p 20003 -sp 20000
-start "train" cmd /K python robots/train.py -p 20004 -sp 20000
-start "wallbanger" cmd /K python robots/wallbanger.py -p 20005 -sp 20000
+start "hideincorner" cmd /K py -3 robots/hideincorner.py -p 20002 -sp 20000
+start "lighthouse" cmd /K py -3 robots/lighthouse.py -p 20003 -sp 20000
+start "train" cmd /K py -3 robots/train.py -p 20004 -sp 20000
+start "wallbanger" cmd /K py -3 robots/wallbanger.py -p 20005 -sp 20000
 
-start "NetBot-Viewer" cmd /K python src/netbots_viewer.py -p 20001 -sp 20000
+start "NetBot-Viewer" cmd /K py -3 src/netbots_viewer.py -p 20001 -sp 20000
