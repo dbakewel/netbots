077fd50cf16fa44442d013bb3a9a97d424073e86
mcstoer <49734282+mcstoer@users.noreply.github.com>
2019-04-25 22:57:11 -0700

    Properly Normalizes Angle
    Normalizes the angle only when the fldspec matches an angles requirements

--- src/netbots_ipc.py
+++ src/netbots_ipc.py
@@ -80,11 +80,14 @@ def isValidMsg(msg):
                     if not isinstance(msg[fld],eval(fldspec[0])):
                         log("Msg '" + fld + "' key has value of type "+str(type(msg[fld]))+" but expected "+fldspec[0]+": " + str(msg), "ERROR")
                         return False
+                    
                     if msg[fld] < fldspec[1] or msg[fld] > fldspec[2]:
-                        msg[fld] = nbmath.normalizeAngle(msg[fld])
-                            if msg[fld] < fldspec[1] or msg[fld] > fldspec[2]:
-                                log("Msg '" + fld + "' key has a value "+str(msg[fld])+" which is out of range ["+str(fldspec[1])+","+str(fldspec[2])+"] : " + str(msg), "ERROR")
-                                return False
+                        if fldspec[2] == math.pi*2 and fldspec[1] == 0:
+                            msg[fld] = nbmath.normalizeAngle(msg[fld])
+                        
+                        else:
+                            log("Msg '" + fld + "' key has a value "+str(msg[fld])+" which is out of range ["+str(fldspec[1])+","+str(fldspec[2])+"] : " + str(msg), "ERROR")
+                            return False
                 else:
                     if not isinstance(msg[fld],eval(fldspec)):
                         log("Msg '" + fld + "' key has value of type "+str(type(msg[fld]))+" but expected "+fldspec+": " + str(msg), "ERROR")
