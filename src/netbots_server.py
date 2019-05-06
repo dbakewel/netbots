cc939b3a714a77b28a96cc1a35da9d5fae4001dd
Douglas Bakewell <11411338+dbakewel@users.noreply.github.com>
2019-05-05 22:35:54 -0700

    Use correct time for computing stat.

--- src/netbots_server.py
+++ src/netbots_server.py
@@ -663,7 +663,7 @@ def logScoreBoard(d):
             "\n              Messages Dropped: " + str(d.state['dropCount']) +\
             "\n             Messages / Second: " + '%.3f' % ((d.srvSocket.recv + d.srvSocket.recv) / float(time.time() - d.state['startTime'])) +\
             "\n         Time Processing Steps: " + '%.3f' % (d.state['stepTime']) + " secs." +\
-            "\n                Steps / Second: " + '%.3f' % (d.state['serverSteps'] / float(time.time() - d.state['startTime'])) +\
+            "\n                Steps / Second: " + '%.3f' % (d.state['serverSteps'] / float(time.time() - d.state['tourStartTime'])) +\
             "\n                 Time Sleeping: " + '%.3f' % (float(d.state['sleepTime'])) + " secs." +\
             "\n            Average Sleep Time: " + '%.6f' % (float(d.state['sleepTime']) / max(1, d.state['sleepCount'])) + " secs." +\
             "\n     Steps Slower Than stepSec: " + str(d.state['longStepCount']) + f" ({float(d.state['longStepCount']) / float(d.state['serverSteps']) * 100.0:>4.2f}%)" +\
