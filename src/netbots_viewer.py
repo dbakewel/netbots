ed6c96b7b2c7fad73cb9a7b0277d3ea83db3d492
Douglas Bakewell <dbakewel@gmail.com>
2019-06-04 21:45:48 -0700

    Update format of func and var names to match netbots style.

--- src/netbots_viewer.py
+++ src/netbots_viewer.py
@@ -52,21 +52,21 @@ class ViewerData():
     replaySaveSteps = math.ceil(replaySec / replayStepSec)
 
 
-def color_variant(hex_color, brightness_offset=1):
+def colorVariant(hexColor, brightnessOffset=1):
     """ takes a color like #87c95f and produces a lighter or darker variant """
-    if len(hex_color) != 7:
-        raise Exception("Passed %s into color_variant(), needs to be in #87c95f format." % hex_color)
-    rgb_hex = [hex_color[x:x+2] for x in [1, 3, 5]]
-    new_rgb_int = [int(hex_value, 16) + brightness_offset for hex_value in rgb_hex]
-    new_rgb_int = [min([255, max([0, i])]) for i in new_rgb_int] # make sure new values are between 0 and 255
+    if len(hexColor) != 7:
+        raise Exception("Passed %s into colorVariant(), needs to be in #87c95f format." % hexColor)
+    rgbHex = [hexColor[x:x+2] for x in [1, 3, 5]]
+    newRGBInt = [int(hexValue, 16) + brightnessOffset for hexValue in rgbHex]
+    newRGBInt = [min([255, max([0, i])]) for i in newRGBInt] # make sure new values are between 0 and 255
     
-    h = "#"
-    for i in new_rgb_int:
+    hexstr = "#"
+    for i in newRGBInt:
         if i < 16:
-            h += "0"
+            hexstr += "0"
         # hex() produces "0x88" or "0x8", we want just "88" or "8"
-        h += hex(i)[2:]
-    return h
+        hexstr += hex(i)[2:]
+    return hexstr
 
 def checkForUpdates(d):
     msg = {"type": "Error", "result": "We never got any new data from server."}
@@ -165,9 +165,9 @@ def checkForUpdates(d):
                 d.botCanon[src] = d.canvas.create_line(0, 0, 50, 50, width=
                     d.conf['botRadius'] * (1/3.0), fill=c)
                 d.botRequestedDirection[src] = d.canvas.create_line(0, 0, 50, 50, width=
-                    d.conf['botRadius'] * (5 / 24.0), arrow=t.LAST, fill=color_variant(c,-100))
+                    d.conf['botRadius'] * (5 / 24.0), arrow=t.LAST, fill=colorVariant(c,-100))
                 d.botCurrentDirection[src] = d.canvas.create_line(0, 0, 50, 50, width=
-                    d.conf['botRadius'] * (5 / 24.0), arrow=t.LAST, fill=color_variant(c,100))
+                    d.conf['botRadius'] * (5 / 24.0), arrow=t.LAST, fill=colorVariant(c,100))
 
             # update text for each bot
             d.botStatusWidgets[src].config(text=bot['name'] +
